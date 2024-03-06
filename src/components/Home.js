d0eb98f1e4fa61912046999f24989ed05d4ae19b
Rubeel Saleem <irubeel@BeelsMacbook.local>
2024-03-06 18:30:43 -0500

    fixed issue

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 export default function Home() {
     return (
         <div className="home-container">
+            <h1 className="home-title">Explore the Universe</h1>
+            <p className="home-tagline">Journey through the cosmos with NASA's Astronomy Picture of the Day</p>
             <Link to='/nasaphoto' className="home-link">See into the Stars!</Link>
-            {/* <Link to='/viewjwst' className="home-link">New James Webb Discoveries!</Link> */}
         </div>
     );
 }
